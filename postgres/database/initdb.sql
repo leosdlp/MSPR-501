c0721cdf84a5d2a37387f08799a525d7f22291f8
Yanis <yanis.marsi.34@gmail.com>
2024-12-31 11:38:48 +0100

    update script sql + dbname

--- postgres/database/initdb.sql
+++ postgres/database/initdb.sql
@@ -1,10 +1,17 @@
-CREATE DATABASE mydatabase;
+DO $$ 
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'projet_mspr') THEN
+      CREATE DATABASE projet_mspr;
+   END IF;
+END $$;
 
-CREATE TABLE mytable (
+\c projet_mspr
+
+CREATE TABLE IF NOT EXISTS mytable (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
 INSERT INTO mytable (name) VALUES ('John Doe');
 INSERT INTO mytable (name) VALUES ('Jane Smith');
-INSERT INTO mytable (name) VALUES ('Alice Johnson');    
+INSERT INTO mytable (name) VALUES ('Alice Johnson');

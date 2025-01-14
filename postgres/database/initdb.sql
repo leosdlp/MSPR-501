33a2a8899e662b49d0cced94d652f82089bc7904
Yanis <yanis.marsi.34@gmail.com>
2025-01-14 15:03:29 +0100

    update initdb.sql

--- postgres/database/initdb.sql
+++ postgres/database/initdb.sql
@@ -10,6 +10,7 @@ END $$;
 CREATE TABLE disease(
    id_disease SERIAL,
    name VARCHAR(50)  NOT NULL,
+   is_pandemic BOOLEAN NOT NULL,
    PRIMARY KEY(id_disease)
 );
 
@@ -51,13 +52,35 @@ CREATE TABLE continent(
    PRIMARY KEY(id_continent)
 );
 
+CREATE TABLE climat_type(
+   id_climat_type SERIAL,
+   name VARCHAR(50)  NOT NULL,
+   description VARCHAR(50) ,
+   PRIMARY KEY(id_climat_type)
+);
+
 CREATE TABLE country(
    id_country SERIAL,
    name VARCHAR(50)  NOT NULL,
    population INTEGER NOT NULL,
+   pib MONEY,
+   id_climat_type INTEGER NOT NULL,
    id_continent INTEGER NOT NULL,
    PRIMARY KEY(id_country),
+   FOREIGN KEY(id_climat_type) REFERENCES climat_type(id_climat_type),
    FOREIGN KEY(id_continent) REFERENCES continent(id_continent)
+);
+
+CREATE TABLE weather_report(
+   id_weather_report SERIAL,
+   date_start DATE NOT NULL,
+   date_end DATE NOT NULL,
+   average_temperature NUMERIC(15,2)  ,
+   average_wind_velocity NUMERIC(15,2)  ,
+   humidity_level NUMERIC(15,2)  ,
+   id_country INTEGER NOT NULL,
+   PRIMARY KEY(id_weather_report),
+   FOREIGN KEY(id_country) REFERENCES country(id_country)
 );
 
 CREATE TABLE place_statement(

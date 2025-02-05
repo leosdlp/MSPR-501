021d695d4fe1e90442367ed891cc0dae78602302
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-02-05 22:47:33 +0100

    Create get_connection.py

---
+++ dev/app/src/db/get_connection.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv # type:ignore
+import os
+import psycopg2 # type:ignore
+
+load_dotenv()
+
+def get_connection():
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_DATABASE"),
+        port=os.getenv("DB_PORT")
+    )

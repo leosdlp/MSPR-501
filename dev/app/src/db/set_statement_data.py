03a064e8378b9d777c9634c82cee060226e8d4ea
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-02-05 22:47:10 +0100

    Create set_statement_data.py

---
+++ dev/app/src/db/set_statement_data.py
@@ -0,0 +1,24 @@
+from psycopg2.extras import execute_batch  # type: ignore
+
+from db.get_connection import get_connection
+
+def set_statement_data(df):
+    if df.empty:
+        raise Exception("[ERROR] Empty DataFrame")
+    
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    sql = """
+        INSERT INTO statement (_date, confirmed, deaths, recovered, active, total_tests, id_disease)
+        VALUES (%s, %s, %s, %s, %s, %s, %s);
+    """
+
+    values = df[["_date", "confirmed", "deaths", "recovered", "active", "total_tests", "id_disease"]].values.tolist()
+    
+    execute_batch(cursor, sql, values)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+

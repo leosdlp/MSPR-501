109c1f3ce8ab2f89f8a020d74ba225da5324d610
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-02-05 22:47:07 +0100

    create truncate_table function

---
+++ dev/app/src/db/truncate_table.py
@@ -0,0 +1,13 @@
+from db.get_connection import get_connection
+
+
+def truncate_table(table):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    sql = f"TRUNCATE TABLE {table} RESTART IDENTITY CASCADE;"
+
+    cursor.execute(sql)
+    conn.commit()
+    cursor.close()
+    conn.close()

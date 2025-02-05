999d31a27306bfc9c669a87a72387685fbc9b7fe
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-02-05 22:47:16 +0100

    Create set_disease.py

---
+++ dev/app/src/db/set_disease.py
@@ -0,0 +1,23 @@
+from psycopg2.extras import execute_batch  # type: ignore
+
+from db.get_connection import get_connection
+from db.truncate_table import truncate_table
+
+
+def set_disease(disease):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    truncate_table("disease")
+
+    sql = "INSERT INTO disease (id_disease, name, is_pandemic) VALUES (%s, %s, %s)"
+
+    values = [
+        (data["id"], name, data["isPandemic"]) 
+        for name, data in disease.items()
+    ]
+
+    execute_batch(cursor, sql, values)
+    conn.commit()
+    cursor.close()
+    conn.close()

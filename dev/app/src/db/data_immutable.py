83b735091cb00b757622fb8e454323e8f0d052ff
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-02-06 17:27:36 +0100

    add set_climat_type into data_immutable

--- dev/app/src/db/data_immutable.py
+++ dev/app/src/db/data_immutable.py
@@ -1,8 +1,12 @@
 from db.countries import set_data_countries
 from db.continents import set_data_continents
+from db.climat_type import set_climat_type
 
 
 def set_data_immutable():
+    print(" ========== Insertion des données dans climat_type ========== ")
+    set_climat_type()
+
     print(" ========== Insertion des données dans continent ========== ")
     set_data_continents()
 

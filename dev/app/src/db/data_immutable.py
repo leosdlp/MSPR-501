05b4cda27fe475153ddac288dd51fae0f3369dfd
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-02-08 22:06:06 +0100

    add set_region and set_data_countries

--- dev/app/src/db/data_immutable.py
+++ dev/app/src/db/data_immutable.py
@@ -1,14 +1,18 @@
 from db.countries import set_data_countries
 from db.continents import set_data_continents
 from db.climat_type import set_climat_type
+from db.region import set_region
 
 
 def set_data_immutable():
-    print(" ========== Insertion des données dans climat_type ========== ")
+    print("\n ===== Insertion des données dans climat_type ===== ")
     set_climat_type()
 
-    print(" ========== Insertion des données dans continent ========== ")
+    print("\n ===== Insertion des données dans continent ===== ")
     set_data_continents()
 
-    print(" ========== Insertion des données dans country ========== ")
+    print("\n ===== Insertion des données dans region ===== ")
+    set_region()
+
+    print("\n ===== Insertion des données dans country ===== ")
     set_data_countries()

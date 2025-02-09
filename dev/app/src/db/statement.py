d39e45f968048fef6db41b5a2148ccdd2f506a50
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-02-09 15:29:31 +0100

    put spark for the insert on db

--- dev/app/src/db/statement.py
+++ dev/app/src/db/statement.py
@@ -1,24 +1,27 @@
 from psycopg2.extras import execute_batch  # type: ignore
 
 from db.connection import get_connection
+from spark.spark import spark_session
 
-def set_statement_data(df):
-    if df.empty:
-        raise Exception("[ERROR] Empty DataFrame")
-    
+def set_statement_data(df_final):
+    spark = spark_session()
+
     conn = get_connection()
     cursor = conn.cursor()
 
-    sql = """
-        INSERT INTO statement (_date, confirmed, deaths, recovered, active, total_tests, id_disease)
-        VALUES (%s, %s, %s, %s, %s, %s, %s);
-    """
+    jdbc_url = "jdbc:postgresql://postgres:5432/mspr501"
+    properties = {
+        "user": "mspr501",
+        "password": "s5t4v5",
+        "driver": "org.postgresql.Driver"
+    }
 
-    values = df[["_date", "confirmed", "deaths", "recovered", "active", "total_tests", "id_disease"]].values.tolist()
-    
-    execute_batch(cursor, sql, values)
+    df_final.write \
+        .jdbc(url=jdbc_url, table="statement", mode="append", properties=properties)
 
-    conn.commit()
     cursor.close()
     conn.close()
+    
+    spark.stop()
 
+    print(f"[INFO] Lignes insérées dans la table 'statement'.")

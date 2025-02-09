b6b121d0661b5a4cfe53f23ddd86e339feb7e584
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-02-09 15:29:34 +0100

    Update clean_covid.py

--- dev/app/src/clean/clean_covid.py
+++ dev/app/src/clean/clean_covid.py
@@ -1,5 +1,46 @@
-import pandas as pd  # type: ignore
+import os
+from spark.spark import spark_session
+from pyspark.sql.functions import udf, lit, col         # type: ignore
+from pyspark.sql.types import IntegerType               # type: ignore
+
+from db.connection import get_connection
 
 
 def clean_covid():
-    return pd.DataFrame()
+
+    spark = spark_session()
+
+    df = spark.read.csv("data_files/corona-virus-report/covid_19_clean_complete.csv", header=True, inferSchema=True)
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT id_country, name FROM country")
+    countries = {name: id_country for id_country, name in cursor.fetchall()}
+
+    def get_country_id(name):
+        return countries.get(name, None)
+
+    get_country_id_udf = udf(get_country_id, IntegerType())
+
+    df = df.withColumn("id_country", get_country_id_udf(col("Country/Region")))
+
+    cursor.execute("SELECT id_disease FROM disease WHERE name = 'covid'")
+    id_disease = cursor.fetchone()[0]
+
+    df = df.withColumn("id_disease", lit(id_disease).cast(IntegerType()))
+
+    df_final = df.select(
+        col("Date").alias("_date"),
+        col("Confirmed").cast(IntegerType()),
+        col("Deaths").cast(IntegerType()),
+        col("Recovered").cast(IntegerType()),
+        col("Active").cast(IntegerType()),
+        col("id_disease"),
+        col("id_country")
+    )
+
+    cursor.close()
+    conn.close()
+    
+    return df_final

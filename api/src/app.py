8e8cbbebbd0f5f9389d742e838454bee4cdeab08
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-02-20 21:31:28 +0100

    Update app.py

--- api/src/app.py
+++ api/src/app.py
@@ -84,4 +84,4 @@ for blueprint in blueprints:
     app.register_blueprint(blueprint, url_prefix='/api')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
